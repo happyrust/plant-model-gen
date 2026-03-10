d162c890abb7516666fe29a89af4c5827f2e1743
dongpengcheng <happydpc@sina.com>
2026-03-10 22:22:04 +0800

    fix(ci): use tuple access for boolean positive geos

--- src/fast_model/gen_model/manifold_bool.rs
+++ src/fast_model/gen_model/manifold_bool.rs
@@ -1329,11 +1329,11 @@ async fn apply_boolean_for_query(
 
     for pos in query.pos_geos.iter() {
 
-        let pos_mesh_id = pos.id.to_mesh_id();
+        let pos_mesh_id = pos.0.to_mesh_id();
 
         // 正实体使用局部变换
 
-        let pos_local_mat = pos.trans.0.to_matrix().as_dmat4();
+        let pos_local_mat = pos.1.to_matrix().as_dmat4();
 
         println!(
 
@@ -1765,9 +1765,9 @@ async fn apply_boolean_for_query(
 
         for pos in query.pos_geos.iter() {
 
-            let pos_mesh_id = pos.id.to_mesh_id();
+            let pos_mesh_id = pos.0.to_mesh_id();
 
-            let pos_local_mat = pos.trans.0.to_matrix().as_dmat4();
+            let pos_local_mat = pos.1.to_matrix().as_dmat4();
 
             if let Ok(m) = load_manifold(&pos_mesh_id, pos_local_mat, true) {
 

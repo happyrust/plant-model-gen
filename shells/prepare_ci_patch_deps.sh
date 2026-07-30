4c8bb9a497d5d42de225960bfee15c414c8e722c
happyrust <golinuxlove@gmail.com>
2026-07-30 08:48:25 +0800

    fix(ci): use buildable rs-core dependency

--- shells/prepare_ci_patch_deps.sh
+++ shells/prepare_ci_patch_deps.sh
@@ -23,7 +23,7 @@ prepare_repo() {
   git clone --depth 1 --branch "$branch" "$repo_url" "$target_dir"
 }
 
-prepare_repo "https://github.com/happyrust/rs-core.git" "codex/fix-refno-enum-cata-hash" \
+prepare_repo "https://github.com/happyrust/rs-core.git" "codex/fix-refno-enum-cata-hash-ci" \
   "$WORKSPACE_PARENT/rs-core"
 prepare_repo "https://github.com/happyrust/pdms-io.git" "dev-3.1" \
   "$WORKSPACE_PARENT/pdms-io-fork"
